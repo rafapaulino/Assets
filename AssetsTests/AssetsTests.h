1cc9721b8f6e74251834bc639a4566c1d8f59481
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-09-21 11:40:54 -0300

    Initial Commit

---
+++ AssetsTests/AssetsTests.h
@@ -0,0 +1,13 @@
+//
+//  AssetsTests.h
+//  AssetsTests
+//
+//  Created by Rafael Brigagão Paulino on 21/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AssetsTests : SenTestCase
+
+@end
---
+++ AssetsTests/AssetsTests.m
@@ -0,0 +1,32 @@
+//
+//  AssetsTests.m
+//  AssetsTests
+//
+//  Created by Rafael Brigagão Paulino on 21/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "AssetsTests.h"
+
+@implementation AssetsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AssetsTests");
+}
+
+@end
